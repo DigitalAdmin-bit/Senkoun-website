55b4d79a25e8b580eca917563c123a34e7a2612c
fbn776 <fbn776@gmail.com>
2026-05-07 21:19:20 +0530

    feat: robots.txt

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -16,7 +16,19 @@ export default function robots(): MetadataRoute.Robots {
         rules: [
             {
                 userAgent: "*",
-                allow: "/",
+                allow: [
+                    "/",
+                    "/care-home",
+                    "/care-home/*",
+                    "/supported-living",
+                    "/supported-living/*",
+                    "/care-home/joseph-lodge-care-home",
+                    "/care-home/joseph-nursing-and-care-home",
+                    "/care-home/all",
+                    "/supported-living/all",
+                    "/careers",
+                    "/news/*"
+                ],
                 disallow: ["/api/", "/enquire/success", "/careers/success", "/careers/openings/success"],
             },
         ],
